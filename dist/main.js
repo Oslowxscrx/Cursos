c559b2f71ebca3444c7780a571d88a9a5faa2166
Oslowxscrx <oer.nogales@yavirac.edu.ec>
2023-05-16 06:19:49 -0500

    cursos+

--- dist/main.js
+++ dist/main.js
@@ -3,12 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let students = [];
 let teachers = [];
 let courses = [];
+let activities = [];
+let gradesbooks = [];
 var Carrera;
 (function (Carrera) {
     Carrera["turismo"] = "Turismo";
     Carrera["marketing"] = "Marketing";
     Carrera["software"] = "Software";
 })(Carrera || (Carrera = {}));
+var Courses;
+(function (Courses) {
+    Courses["typescript"] = "Typescript";
+    Courses["php"] = "PHP";
+    Courses["angular"] = "Angular";
+})(Courses || (Courses = {}));
 function addStudent() {
     let currentStudent = {
         name: readHtml("nameStudent"),
@@ -45,4 +53,30 @@ function addCourse() {
 }
 function readHtml(id) {
     return document.getElementById(id).value;
+}
+function addActivity() {
+    let currentActivity = {
+        name: readHtml("activity")
+    };
+    activities.push(currentActivity);
+    console.table(activities);
+}
+function addgradeBook() {
+    let currentGradeBook = {
+        course: readHtml("coursegrd"),
+        activity: readHtml("activitygrd"),
+        peso: parseInt(readHtml("weigthgrd")),
+        gradeMax: parseInt(readHtml("maxnotagrd"))
+    };
+    gradesbooks.push(currentGradeBook);
+    console.table(gradesbooks);
+}
+function getOptions() {
+    let currentcourseone = document.getElementById("coursegrd");
+    document.querySelectorAll("#coursegrd option").forEach(option => option.remove);
+    let coursevar = Object.values(courses);
+    coursevar.forEach(element => {
+        let option = document.createElement("option");
+        option.value = element.name;
+    });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-import {Course} from "./entities/course";
 import{Students} from "./entities/students"
 import{Teachers} from "./entities/teacher"
-
+import {Course} from "./entities/course";
+import { Activity } from "./entities/activity";
+import { GradesBook } from "./entities/gradesBook";
 
 let students: Students[] = [];
 let teachers: Teachers[] = [];
 let courses: Course[] = [];
-
+let activities: Activity[] = [];
+let gradesbooks: GradesBook[] =[];
 enum Carrera {
     turismo = "Turismo",
     marketing = "Marketing",
     software = "Software",
 }
-
-
-
-
+enum  Courses{
+   typescript = "Typescript",
+   php = "PHP",
+   angular = "Angular",
+}
 function addStudent(){
  let currentStudent: Students = {
     name: readHtml("nameStudent"),
@@ -28,7 +31,6 @@ function addStudent(){
  console.log(students);
  console.table(students);
 }
-
 function addTeacher(){
     let currentTeacher: Teachers = {
        name: readHtml("nameTeacher"),
@@ -37,25 +39,52 @@ function addTeacher(){
        title:readHtml("titleTeacher"),
        asignature: readHtml("asignatureTeacher") as "Interfaces" | "Programacion" | "Metodologias",
        carrer: readHtml("carrerTeacher"),
-
     }
     teachers.push(currentTeacher);
     console.log(teachers);
     console.table(teachers);
    }
-
    function addCourse(){
     let currentCourse: Course = {
        area: readHtml("areaCourse"),
        name:  readHtml("nameCourse"),
-
-
     }
     courses.push(currentCourse);
     console.log(courses);
     console.table(courses);
    }
+
+   
 function readHtml(id: string): string{
     return (<HTMLInputElement> document.getElementById(id)).value;
 }
 
+function addActivity() {
+   let currentActivity: Activity = {
+      name: readHtml("activity")
+   }
+   activities.push(currentActivity);
+   console.table(activities);
+}
+
+function addgradeBook(){
+   let currentGradeBook: GradesBook ={
+      course: readHtml("coursegrd"),
+      activity: readHtml("activitygrd"),
+      peso: parseInt(readHtml("weigthgrd")),
+      gradeMax: parseInt(readHtml("maxnotagrd"))      
+   }
+   gradesbooks.push(currentGradeBook);
+   console.table(gradesbooks);
+}
+
+function getOptions():void{
+   let currentcourseone = document.getElementById("coursegrd") as HTMLSelectElement;
+   document.querySelectorAll("#coursegrd option").forEach(option => option.remove);
+
+   let coursevar = Object.values(courses);
+   coursevar.forEach(element => {
+      let option = document.createElement("option");
+      option.value = element.name;
+   });
+}
